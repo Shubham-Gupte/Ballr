032a5c3ec48df4d138c9161c72cc4a9a2b188617
asuasuhyuu <ahughes44@gatech.edu>
2017-10-14 21:39:04 -0400

    finished contact page; doing about page

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
+import { ProfilePage } from '../pages/profile/profile';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -16,6 +17,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
+    ProfilePage,
     HomePage,
     TabsPage
   ],
@@ -28,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
+    ProfilePage,
     HomePage,
     TabsPage
   ],
