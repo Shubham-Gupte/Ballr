f35cc4fb6783f792d89d32d3cefc20e35ec13ce9
asuasuhyuu <ahughes44@gatech.edu>
2017-10-15 04:38:05 -0400

    Finished Med page with sub pages

---
+++ front-end/src/pages/about/about-pages/about-ballr/about-ballr.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about-ballr',
+  templateUrl: 'about-ballr.html'
+})
+export class AboutBallrPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ front-end/src/pages/about/about-pages/self-sex/self-sex.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-self-sex',
+  templateUrl: 'self-sex.html'
+})
+export class SelfSexPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
