7a32c67119b4a58cf6fa8283eb28710ef571c697
Brian Cabigon-Soliber <brian.cabigonsoliber@ge.com>
2017-10-15 00:19:21 -0400

    Added global font

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { ProfilePage } from '../pages/profile/profile';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { TodayPage } from '../pages/today/today';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     ProfilePage,
     HomePage,
-    TabsPage
+    TabsPage, 
+    TodayPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     ProfilePage,
     HomePage,
-    TabsPage
+    TabsPage, 
+    TodayPage
   ],
   providers: [
     StatusBar,
--- front-end/src/pages/tabs/tabs.ts
+++ front-end/src/pages/tabs/tabs.ts
@@ -3,15 +3,18 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { TodayPage } from '../today/today';
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
+  tab1Root = TodayPage;
   tab2Root = ContactPage;
   tab3Root = AboutPage;
+  tab4Root = ProfilePage;
 
   constructor() {
 
